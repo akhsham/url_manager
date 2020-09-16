2f6907bd7357723e76f3d24c2dcf7f9c449c8e7e
darian <akhsham@gmail.com>
2020-09-16 18:10:20 +0430

    Add expire_date in models.py

--- url_shortner/link_services/models.py
+++ url_shortner/link_services/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Links (models.Model):
     input_link = models.TextField(blank=False)
-    input_text = models.TextField(default="TEXT",null=False,blank=False)
-    output_link_30days = models.CharField (default= "text", max_length= 30, unique=True, blank=False)
-    output_link_90days = models.CharField (default= "text",max_length= 30, unique=True, blank=False)
-    output_link_premium = models.CharField (default= "text",max_length= 30, unique=True, blank=False)
+    input_text = models.TextField(default="TEXT", null=False, blank=False)
+    expire_date = models.DateTimeField()
 
- #   user = models.ForeignKey (User,on_delete=models.CASCADE(),)
+    output_link_30days = models.CharField(default= "text", max_length= 30, unique=True, blank=False)
+    output_link_90days = models.CharField(default= "text", max_length= 30, unique=True, blank=False)
+    output_link_premium = models.CharField(default= "text", max_length= 30, unique=True, blank=False)
+#   user = models.ForeignKey (User,on_delete=models.CASCADE(),)
 
 
-class Characteristic  (models.Model):
+class Statistics  (models.Model):
 
     lifespan = models.DateTimeField()
     created_date = models.DateTimeField(auto_now_add=True)
-    expire_date = models.DateTimeField()
     click_counts = models.IntegerField(default=0)
 
 
